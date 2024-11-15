0c240ce1c43c5c0d75ece211ecbf468b42a9dab8
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-11-15 18:23:22 +0530

    env update

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./connections/dbconnect');
+const dotenv = require('dotenv');
+dotenv.config();
+connectDB();
+
+
+app.use('/api/barisdogonsutcu', require('./routes/barisdogonsutcu.js'));
+app.use('/api/htwarriors', require('./routes/htwarriors.js'));
+app.use('/api/simontheonlyone', require('./routes/simontheonlyone.js'));
+app.use('/api/souravbandyopadhyay', require('./routes/souravBandyopadhyay.js'));
+app.use('/api/trayrandall', require('./routes/trayRandall.js'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    });
+        
