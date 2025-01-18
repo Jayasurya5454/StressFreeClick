2a04fe62f2bf2e969b9b5558191a109e729c3ef2
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2025-01-18 09:22:11 +0530

    loding spinner added

--- frontend/src/components/likeme.jsx
+++ frontend/src/components/likeme.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Checkbox from '../components/barisdogonsutcu/Checkbox';
 import Checkbox1 from '../components/htwarriors/Checkbox';
 import Checkbox2 from '../components/simontheonlyone/Checkbox';
 import Checkbox3 from '../components/souravBandyopadhyay/Checkbox';
 import Checkbox4 from '../components/trayRandall/Checkbox';
+import LoadingSpinner from '../pages/LoadingSpinner'; 
+import axios from '../utils/axiosConfig';
+
 const Likeme = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkBackendStatus = async () => {
+      try {
+        const response = await axios.get('/status');
+        console.log(response); 
+        if (response.data.status === 'ready') {
+          setIsLoading(false); 
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        setTimeout(checkBackendStatus, 1000); 
+      }
+    };
+
+    checkBackendStatus();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-around', 
-        alignItems: 'center',
-        marginTop: '150px',
-        flexWrap: 'wrap', 
-        gap: '20px', 
-        width: '80%', 
-        boxSizing: 'border-box', 
-      }}
-    >
+    <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginTop: '150px', flexWrap: 'wrap', gap: '20px', width: '80%', boxSizing: 'border-box' }}>
       <div style={{ textAlign: 'center' }}>
         <Checkbox1 />
       </div>
@@ -33,7 +52,6 @@ const Likeme = () => {
       <div style={{ textAlign: 'center' }}>
         <Checkbox4 />
       </div>
-      
     </div>
   );
 };
