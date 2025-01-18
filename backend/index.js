82d3c993654d33503161a7950d3c7630fbf4895b
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2025-01-18 09:22:48 +0530

    handshaking function added

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,9 @@ app.use('/api/htwarriors', require('./routes/htwarriors.js'));
 app.use('/api/simontheonlyone', require('./routes/simontheonlyone.js'));
 app.use('/api/souravbandyopadhyay', require('./routes/souravBandyopadhyay.js'));
 app.use('/api/trayrandall', require('./routes/trayRandall.js'));
-
+app.get('/api/status', (req, res) => {
+    res.json({ status: 'ready' });
+  });
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
     });
